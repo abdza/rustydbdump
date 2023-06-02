dab98b70f1aada21cd00995fd3f4bd0f532b11dd
abdza <abdullah.zainul@gmail.com>
2023-06-03 07:34:00 +0800

    Reformatted after cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use async_std::net::TcpStream;
+use rust_xlsxwriter::*;
+use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use tiberius::{Client, Config, Query, AuthMethod, ColumnType, numeric::Numeric, time};
-use async_std::net::TcpStream;
-use rust_xlsxwriter::*;
-use serde::{Serialize, Deserialize};
+use tiberius::{numeric::Numeric, time, AuthMethod, Client, ColumnType, Config, Query};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Settings {
@@ -21,8 +21,10 @@ async fn main() -> Result<(), anyhow::Error> {
     // Using the builder method to construct the options.
     let mut config = Config::new();
 
-    let file_contents = std::fs::read_to_string("settings.json").expect("Should have been able to read the file");
-    let settings: Settings = serde_json::from_str(&file_contents).expect("Should have been able to parse the file");
+    let file_contents =
+        std::fs::read_to_string("settings.json").expect("Should have been able to read the file");
+    let settings: Settings =
+        serde_json::from_str(&file_contents).expect("Should have been able to parse the file");
 
     config.database(settings.database);
     config.host(settings.host);
@@ -79,95 +81,115 @@ async fn main() -> Result<(), anyhow::Error> {
     let mut workbook = Workbook::new();
     let worksheet = workbook.add_worksheet();
 
-    results.first().unwrap().into_iter().enumerate().for_each(|(index_row,row)| {
-        // println!("This is a row!: {:#?}", index_row);
-        if index_row==0 {
-            for (index_column,column) in row.columns().iter().enumerate() {
-                worksheet.write( index_row as u32, index_column as u16 , column.name() );
-            }
-        }
-        for (index_column,column) in row.columns().iter().enumerate() {
-            // let value = row.try_get(index_column);
-            // println!("{:#?}",column);
-            // worksheet.write(index_column as u32 , index_row as u16 , value );
-            match column.column_type() {
-                ColumnType::Numericn => {
-                    // println!("this is a numericcc!!");
-                    let value = row.try_get::<Numeric,usize>(index_column);
-                    if let Some(real_value) = value.unwrap() {
-                        // println!("real value: {:#?}", real_value.value());
-                        worksheet.write( index_row as u32 + 1, index_column as u16 , real_value.value() as i32 );
-                    }
-                    // println!("val:{:#?}",value.unwrap());
-                },
-                ColumnType::Bitn => {
-                    // println!("this is a bitn!!");
-                    let value = row.try_get::<bool,usize>(index_column);
-                    if let Some(real_value) = value.unwrap() {
-                        // println!("real value: {:#?}", real_value);
-                        worksheet.write( index_row as u32 + 1, index_column as u16 , real_value );
-                    }
-                    // println!("val:{:#?}",value.unwrap());
-                },
-                ColumnType::Intn => {
-                    // println!("this is a int!!");
-                    let got_error = false;
-                    let value = row.try_get::<u8,usize>(index_column);
-                    // println!("after u8: {:#?}",value);
-                    let real_value = match value {
-                        Ok(num) => {
-                            if let Some(real_value) = value.unwrap() {
-                                // println!("real value u8: {:#?}", real_value);
-                                worksheet.write( index_row as u32 + 1, index_column as u16 , real_value );
-                            }
-                        },
-                        Err(_) => {
-                            let value = row.try_get::<i64,usize>(index_column);
-                            // println!("after i64: {:#?}",value);
-                            let real_value = match value {
-                                Ok(num) => {
-                                    if let Some(real_value) = value.unwrap() {
-                                        // println!("real value i64: {:#?}", real_value);
-                                        worksheet.write( index_row as u32 + 1, index_column as u16 , real_value as i32 );
-                                    }
-                                },
-                                Err(_) => continue,
-                            };
-
-                        },
-                    };
-                    // println!("after match: {:#?}",real_value);
-                    // if let Some(real_value) = value.unwrap() {
-                    //     println!("real value: {:#?}", real_value);
-                    //     // worksheet.write( index_row as u32, index_column as u16 , real_value.value() as i32 );
-                    // }
-                    // println!("val:{:#?}",value.unwrap());
-                },
-                ColumnType::BigVarChar | ColumnType::NVarchar => {
-                    // println!("this is a string!!");
-                    let value = row.try_get::<&str,usize>(index_column);
-                    if let Some(real_value) = value.unwrap() {
-                        // println!("real value: {:#?}", real_value);
-                        worksheet.write( index_row as u32 + 1, index_column as u16 , real_value );
-                    }
-                    // println!("val:{:#?}",value.unwrap());
-                },
-                ColumnType::Datetime2 | ColumnType::Datetimen => {
-                    // println!("this is a datetime!!");
-                    let value = row.try_get::<time::chrono::NaiveDateTime,usize>(index_column);
-                    if let Some(real_value) = value.unwrap() {
-                        // println!("real value: {:#?}", real_value.to_string());
-                        worksheet.write(index_row as u32 + 1, index_column as u16 , real_value.to_string() );
-                    }
-                    // println!("val:{:#?}",value.unwrap());
-                },
-                _ => {
-                    println!("i dont know what this is");
-                    println!("{:#?}",column);
+    results
+        .first()
+        .unwrap()
+        .into_iter()
+        .enumerate()
+        .for_each(|(index_row, row)| {
+            // println!("This is a row!: {:#?}", index_row);
+            if index_row == 0 {
+                for (index_column, column) in row.columns().iter().enumerate() {
+                    let _ = worksheet.write(index_row as u32, index_column as u16, column.name());
                 }
             }
-        }
-    });
+            for (index_column, column) in row.columns().iter().enumerate() {
+                // let value = row.try_get(index_column);
+                // println!("{:#?}",column);
+                // worksheet.write(index_column as u32 , index_row as u16 , value );
+                match column.column_type() {
+                    ColumnType::Numericn => {
+                        // println!("this is a numericcc!!");
+                        let value = row.try_get::<Numeric, usize>(index_column);
+                        if let Some(real_value) = value.unwrap() {
+                            // println!("real value: {:#?}", real_value.value());
+                            let _ = worksheet.write(
+                                index_row as u32 + 1,
+                                index_column as u16,
+                                real_value.value() as i32,
+                            );
+                        }
+                        // println!("val:{:#?}",value.unwrap());
+                    }
+                    ColumnType::Bitn => {
+                        // println!("this is a bitn!!");
+                        let value = row.try_get::<bool, usize>(index_column);
+                        if let Some(real_value) = value.unwrap() {
+                            // println!("real value: {:#?}", real_value);
+                            let _ = worksheet.write(index_row as u32 + 1, index_column as u16, real_value);
+                        }
+                        // println!("val:{:#?}",value.unwrap());
+                    }
+                    ColumnType::Intn => {
+                        // println!("this is a int!!");
+                        let got_error = false;
+                        let value = row.try_get::<u8, usize>(index_column);
+                        // println!("after u8: {:#?}",value);
+                        let real_value = match value {
+                            Ok(num) => {
+                                if let Some(real_value) = value.unwrap() {
+                                    // println!("real value u8: {:#?}", real_value);
+                                    worksheet.write(
+                                        index_row as u32 + 1,
+                                        index_column as u16,
+                                        real_value,
+                                    );
+                                }
+                            }
+                            Err(_) => {
+                                let value = row.try_get::<i64, usize>(index_column);
+                                // println!("after i64: {:#?}",value);
+                                let real_value = match value {
+                                    Ok(num) => {
+                                        if let Some(real_value) = value.unwrap() {
+                                            // println!("real value i64: {:#?}", real_value);
+                                            worksheet.write(
+                                                index_row as u32 + 1,
+                                                index_column as u16,
+                                                real_value as i32,
+                                            );
+                                        }
+                                    }
+                                    Err(_) => continue,
+                                };
+                            }
+                        };
+                        // println!("after match: {:#?}",real_value);
+                        // if let Some(real_value) = value.unwrap() {
+                        //     println!("real value: {:#?}", real_value);
+                        //     // worksheet.write( index_row as u32, index_column as u16 , real_value.value() as i32 );
+                        // }
+                        // println!("val:{:#?}",value.unwrap());
+                    }
+                    ColumnType::BigVarChar | ColumnType::NVarchar => {
+                        // println!("this is a string!!");
+                        let value = row.try_get::<&str, usize>(index_column);
+                        if let Some(real_value) = value.unwrap() {
+                            // println!("real value: {:#?}", real_value);
+                            worksheet.write(index_row as u32 + 1, index_column as u16, real_value);
+                        }
+                        // println!("val:{:#?}",value.unwrap());
+                    }
+                    ColumnType::Datetime2 | ColumnType::Datetimen => {
+                        // println!("this is a datetime!!");
+                        let value = row.try_get::<time::chrono::NaiveDateTime, usize>(index_column);
+                        if let Some(real_value) = value.unwrap() {
+                            // println!("real value: {:#?}", real_value.to_string());
+                            worksheet.write(
+                                index_row as u32 + 1,
+                                index_column as u16,
+                                real_value.to_string(),
+                            );
+                        }
+                        // println!("val:{:#?}",value.unwrap());
+                    }
+                    _ => {
+                        println!("i dont know what this is");
+                        println!("{:#?}", column);
+                    }
+                }
+            }
+        });
 
     workbook.save(settings.output);
 
